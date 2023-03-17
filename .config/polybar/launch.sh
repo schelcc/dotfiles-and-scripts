e8a064fd69b11cde3c9cba895432c9217f2afd6b
Cole Scheller <schelcc@pobox.com>
2023-03-16 22:13:33 -0400

    dual monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,19 @@
 
 killall -q polybar
 
+monitor_poll=$(xrandr|grep "DP-2"|grep " disconnected")
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar single --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log
+
+if [[ $monitor_poll == "" ]]; then
+	export LEFT_MONITOR=DP-2
+	export RIGHT_MONITOR=eDP-1
+
+	polybar double-left --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_left.log &
+	polybar double-right --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_right.log &
+else
+	polybar single --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_single.log &
+fi
+
+wallpaper.sh &
+
