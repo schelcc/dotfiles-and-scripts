ac08e18bb8c29af12ee8780270c4053adc9c8c38
Cole Scheller <schelcc@pobox.com>
2023-03-17 19:03:20 -0400

    decreased docked laptop monitor scale

--- bin/ws_dock.sh
+++ bin/ws_dock.sh
@@ -3,8 +3,17 @@
 # Init any newly connected monitors
 xrandr --auto
 
-# Set monitors with presets
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2400x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.25x1.25 --output HDMI-3 --off --output DP-3 --off --output DP-4 --off
+LAPTOP=eDP-1
+EXTERNAL=DP-2
+
+dock_check=$(xrandr --listactivemonitors|grep $EXTERNAL)
+
+if [[ $dock_check == "" ]]; then
+	xrandr --output $LAPTOP --mode 1920x1200 --scale 1.0x1.0
+else
+	xrandr --output $LAPTOP --primary --mode 1920x1200 --pos 2400x0 --rotate normal --scale 0.85x0.85 \
+		--output $EXTERNAL --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.25x1.25
+fi
 
 # Swap caps and LCtrl - Do this because when ws_dock is run there is also a new keyboard attached
 setxkbmap -layout us -option ctrl:swapcaps
