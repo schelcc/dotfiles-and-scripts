ff745f3e5d0f4617ffac7d509e02e95c598087d0
Cole Scheller <schelcc@pobox.com>
2023-05-11 15:59:58 -0400

    resolve dependent config colors within polybar_print

--- .config/polybar/polybar_print.py
+++ .config/polybar/polybar_print.py
@@ -1,14 +1,28 @@
-def polybar_print(msg, foreground_color=None, underline_color=None):
+import configparser
+import os
+from string import punctuation
+
+polybar_config = configparser.ConfigParser()
+polybar_config.read(f"{os.environ['HOME']}/.config/polybar/colors.ini")
+
+def key_resolver(util_key):
+	root, color = polybar_config['util-colors'][util_key].strip(punctuation).split('.')
+	return polybar_config[root][color]
+
+fg = key_resolver('foreground')
+ul = key_resolver('accent')
+
+def polybar_print(msg, fg_key='foreground', ul_key=None):
+	fg_color = key_resolver(fg_key)
+
 	output = ""
 	
-	if underline_color is not None:
-		underline_str = "%{u" + underline_color + "}%{+u}"
+	if ul_key is not None:
+		ul_color = key_resolver(ul_key)
+		underline_str = "%{u" + ul_color + "}%{+u}"
 		output += underline_str
 
-	if foreground_color is not None:
-		foreground_str = "%{F" + foreground_color + "}" + msg + "%{F-}"
-	else:
-		foreground_str = msg
+	foreground_str = "%{F" + fg_color + "}" + msg + "%{F-}"
 	
 	output += foreground_str
 
